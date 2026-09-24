38e58899269fb50efa05689116ebec4233ed840f
Thaddeus Schelp <tschelp@bruinmail.slcc.edu>
2026-09-24 17:19:50 -0600

    add quicksort unit tests

---
+++ test/TestQuickSort.py
@@ -0,0 +1,34 @@
+###################################
+# Coding Cadets                   #
+# Thaddeus Schelp, Todd Dharni    #
+# CS2430                          #
+# Programming Project 1           #
+# Primary Author: Thaddeus Schelp #
+###################################
+
+import unittest
+
+import QuickSort
+
+import Constants
+
+
+class TestQuickSort(unittest.TestCase):
+    def test_already_sorted(self):
+        merge: QuickSort.QuickSort = QuickSort.QuickSort()
+        self.assertEqual(merge.sort(Constants.TEST_CASE_ALREADY_SORTED), Constants.SOLUTION)
+        self.assertGreater(merge._comparison_count, 0)
+
+    def test_reverse_sorted(self):
+        merge: QuickSort.QuickSort = QuickSort.QuickSort()
+        self.assertEqual(merge.sort(Constants.TEST_CASE_REVERSE_SORTED), Constants.SOLUTION)
+        self.assertGreater(merge._comparison_count, 0)
+
+    def test_normal_case(self):
+        merge: QuickSort.QuickSort = QuickSort.QuickSort()
+        self.assertEqual(merge.sort(Constants.TEST_CASE_NORMAL), Constants.SOLUTION)
+        self.assertGreater(merge._comparison_count, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
