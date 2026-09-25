1ef1e78fdf6954b077b7f49d36ecd96b46609f0e
Brayden Graham <braydengraham@gmail.com>
2026-09-24 18:20:25 -0600

    Updated test casees to include me; Created classDiagram.md

---
+++ docs/classDiagram.md
@@ -0,0 +1,18 @@
+```mermaid
+classDiagram
+
+    class MergeSort {
+        - merge (IterableiInt~ l, Iterable~Int~ r) Iterable~int~
+        + sort (MutableSequence~Int~ a) MutableSequence~int~
+    }
+    
+    class HeapSort{
+        - heapify (MutableSequence~int~ a, int node, int unsorted_size)
+        
+    }
+    
+    class QuickSort{}
+    
+    class SortingAlgorithm{}
+    
+```
--- test/Constants.py
+++ test/Constants.py
@@ -1,10 +1,10 @@
-###################################
-# Coding Cadets                   #
-# Thaddeus Schelp, Todd Dharni    #
-# CS2430                          #
-# Programming Project 1           #
-# Primary Author: Thaddeus Schelp #
-###################################
+################################################
+# Coding Cadets                                #
+# Thaddeus Schelp, Todd Dharni, Brayden Graham #
+# CS2430                                       #
+# Programming Project 1                        #
+# Primary Author: Thaddeus Schelp              #
+################################################
 
 SOLUTION = [0, 1, 2, 3]
 TEST_CASE_ALREADY_SORTED = [0, 1, 2, 3]  # already sorted
